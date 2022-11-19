345918a30f3498212239e8dd4ab6790c71c9267a
aditmulyatama <adit.mulyatama211201@gmail.com>
2022-11-19 17:28:24 +0700

    fixing agent screen

--- lib/network/agent_client.dart
+++ lib/network/agent_client.dart
@@ -3,16 +3,22 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
+import 'package:valorant_tips/network/api_client.dart';
 
 import '../models/agent.dart';
 
-class AgentClient {
+class AgentClient extends ApiClient {
   // Get Agents
   Future<Iterable<Agent>> getAgents({String? agentRole}) async {
     Iterable<Agent> agents = [];
-    Uri url = Uri.parse("https://valorant-api.com/v1/agents");
+    Uri url = Uri.parse("${baseUrl}v1/agents");
+    // Params
+    Map<String, String> qParams = {
+      'isPlayableCharacter': 'true',
+    };
     // Get response
-    var apiResult = await http.get(url);
+    final finalUrl = url.replace(queryParameters: qParams);
+    var apiResult = await http.get(finalUrl);
     var jsonObject = jsonDecode(apiResult.body);
     List parsedList = (jsonObject as Map<String, dynamic>)['data'];
     agents = parsedList.map((element) {
@@ -20,7 +26,8 @@ class AgentClient {
     });
 
     if (agentRole != null) {
-      agents = agents.where((element) => element.role!.displayName!.toLowerCase() == agentRole.toLowerCase());
+      agents = agents.where((element) =>
+          element.role!.displayName!.toLowerCase() == agentRole.toLowerCase());
     }
     return agents;
   }
--- lib/network/api_client.dart
+++ lib/network/api_client.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:http/http.dart';
 
 class ApiClient {
   final Dio dio = Dio();
